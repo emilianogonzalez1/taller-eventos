0cd36ce80f000fa16d0250d1669e5c97a79938ee
emigon <emipastor2001@gmail.com>
2023-08-13 20:19:24 -0300

    solucion-click

--- evento.js
+++ evento.js
@@ -1,11 +1,10 @@
 const dividiv = document.getElementById('eldiv')
 
 function saludar(){
-    event.stopPropagation();
     alert('Hola!');
+    event.stopPropagation();
 }
 
 dividiv.addEventListener('click', function(e){
 alert('Hola! Soy el div');
-
 })
